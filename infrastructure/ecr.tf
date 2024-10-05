6493d2e7ec9d76055cbb4d5cf87dd3699ab3ec80
Gabriel Santos <gnascimento.info@gmail.com>
2024-10-05 17:51:55 -0300

    remove unused configuration for ecs, ecr and redshift

--- infrastructure/ecr.tf
+++
@@ -1,7 +0,0 @@
-# resource "aws_ecr_repository" "ecs_task_repo" {
-#   name                 = "ecs-task-repo"
-#   image_tag_mutability = "MUTABLE"
-#   image_scanning_configuration {
-#     scan_on_push = true
-#   }
-# }
--- infrastructure/redshift.tf
+++
@@ -1,9 +0,0 @@
-# resource "aws_redshift_cluster" "demo_redshift" {
-#   cluster_identifier      = "redshift-cluster"
-#   database_name           = "dev"
-#   master_username         = "admin"
-#   master_password         = "Password123456"
-#   node_type               = "dc2.large"
-#   cluster_type            = "single-node"
-#   skip_final_snapshot     = true
-# }
